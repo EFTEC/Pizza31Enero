89a3023fa58762e9b2d82c2858db5fab9f9f7921
sally <alumno@DESKTOP-GKNRL0G>
2019-01-31 11:18:09 -0300

    pp

---
+++ ProyectoWS/dal/EmpleadoDal.cs
@@ -0,0 +1,20 @@
+﻿using ProyectoWS.datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWS.dal
+{
+    public class EmpleadoDal
+    {
+        public static List<EMPLEADOS> Listar()
+        {
+            using (var model2 = new Model2())
+            {
+                return model2.EMPLEADOS.ToList();
+            }
+        }
+
+    }
+}
