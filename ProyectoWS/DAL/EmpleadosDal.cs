4889a1141e4fc603036a268c74d940902ad1d435
davidrioseftec <davidrioseftec@eftec.cl>
2019-01-31 11:13:01 -0300

    Ok Dal

---
+++ ProyectoWS/DAL/EmpleadosDal.cs
@@ -0,0 +1,27 @@
+﻿using ProyectoWS.datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWS.DAL
+{
+    public class EmpleadosDal
+    {
+        public static List<EMPLEADOS> listar()
+        {
+            using (var model2 = new Model2())
+            {
+                return model2.EMPLEADOS.ToList();
+            }
+        }
+        public static void Insertar(EMPLEADOS empleado)
+        {
+            using (var model2 = new Model2())
+            {
+                model2.EMPLEADOS.Add(empleado);
+                model2.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ProyectoWS/DAL/PizzasDal.cs
@@ -0,0 +1,19 @@
+﻿using ProyectoWS.datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWS.DAL
+{
+    public class PizzasDal
+    {
+        public static List<PIZZAS> listar()
+        {
+            using (var model2 = new Model2())
+            {
+                return model2.PIZZAS.ToList();
+            }
+        }
+    }
+}
