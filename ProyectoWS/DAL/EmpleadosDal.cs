a2ee8ad7012024552bbc6820462e16aa6882845b
davidrioseftec <davidrioseftec@eftec.cl>
2019-01-31 11:34:35 -0300

    Actualizar DAL

--- ProyectoWS/DAL/EmpleadosDal.cs
+++ ProyectoWS/DAL/EmpleadosDal.cs
@@ -8,19 +8,20 @@ namespace ProyectoWS.DAL
 {
     public class EmpleadosDal
     {
-        public static List<EMPLEADOS> LeerEmpleado()
+        public static EMPLEADOS LeerEmpleado(string usuario)
+        {
+            var emp = new EMPLEADOS();
+            using (var grupo = new Model2())
+            {
+                emp = grupo.EMPLEADOS.Where(e => e.USUARIO == usuario).First();
+                return emp;
+            }
+        }
+        public static List<EMPLEADOS> ListarEmpleado()
         {
             using (var model2 = new Model2())
             {
                 return model2.EMPLEADOS.ToList();
-            }
-        }
-        public static void ListarEmpleado(EMPLEADOS empleado)
-        {
-            using (var model2 = new Model2())
-            {
-                model2.EMPLEADOS.Add(empleado);
-                model2.SaveChanges();
             }
         }
     }
